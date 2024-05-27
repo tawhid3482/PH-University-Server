83c73e459d45d9cf18e17b1e3058aab140270644
saikat <tawhidulislam3482@gmail.com>
2024-05-27 19:04:17 +0600

    user model

--- src/app/modules/users/user.model.ts
+++ src/app/modules/users/user.model.ts
@@ -1,5 +1,7 @@
 import { Schema, model } from "mongoose";
 import { TUser } from "./user.interface";
+import config from "../../config";
+import bcrypt from 'bcrypt';
 
 const userSchema = new Schema<TUser>({
     id: {
@@ -16,19 +18,43 @@ const userSchema = new Schema<TUser>({
     },
     role: {
         type: String,
-        enum:['student','faculty','admin'],
+        enum: ['student', 'faculty', 'admin'],
     },
     status: {
         type: String,
-        enum:['in-progress','blocked'],
-        default:'in-progress'
+        enum: ['in-progress', 'blocked'],
+        default: 'in-progress'
     },
     isDeleted: {
         type: Boolean,
         default: false
     },
 },
-{
-    timestamps:true
-})
-export const User = model<TUser>('User',userSchema)
+    {
+        timestamps: true
+    })
+
+// pre save middleware/ hook : will work on create()  save()
+userSchema.pre('save', async function (next) {
+    // console.log(this, 'pre hook : we will save  data');
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const user = this; // doc
+    // hashing password and save into DB
+    user.password = await bcrypt.hash(
+        user.password,
+        Number(config.bcrypt_salt_rounds),
+    );
+    next();
+});
+
+// post save middleware / hook
+userSchema.post('save', function (doc, next) {
+    doc.password = '';
+    next();
+});
+
+
+
+
+
+export const User = model<TUser>('User', userSchema)
