235995c0648662db22e0e9160c5b6acef6d20af8
saikat <tawhidulislam3482@gmail.com>
2024-05-27 09:38:29 +0600

    user model ready

---
+++ src/app/modules/users/user.model.ts
@@ -0,0 +1,33 @@
+import { Schema, model } from "mongoose";
+import { TUser } from "./user.interface";
+
+const userSchema = new Schema<TUser>({
+    id: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    needsPasswordChange: {
+        type: Boolean,
+        default: true
+    },
+    role: {
+        type: String,
+        enum:['student','faculty','admin'],
+    },
+    status: {
+        type: String,
+        enum:['in-progress','blocked'],
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
+},
+{
+    timestamps:true
+})
+export const user = model<TUser>('User',userSchema)
---
+++ src/app/modules/users/user.validation.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+const userValidationSchema = z.object({
+    id:z.string(),
+    password:z.string().max(20,{message:'Password con not be more then 20 characters'}),
+    needsPasswordChange:z.boolean().optional().default(true),
+    role:z.enum(['student','faculty','admin']),
+    status:z.enum(['in-progress','blocked']).default('in-progress'),
+    isDeleted:z.boolean().optional().default(false),
+
+})
+
+export const userValidation = {
+    userValidationSchema
+}
