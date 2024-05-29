b79e367439f52d3876ab21f89da5dc8500d310ae
saikat <tawhidulislam3482@gmail.com>
2024-05-29 11:05:02 +0600

    validate Request

--- src/app/modules/student/student.validation.ts
+++ src/app/modules/student/student.validation.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-const userNameSchema = z.object({
+const userNameValidationSchema = z.object({
   firstName: z
     .string()
     .min(1)
@@ -12,7 +12,7 @@ const userNameSchema = z.object({
   lastName: z.string(),
 });
 
-const guardianSchema = z.object({
+const guardianValidationSchema = z.object({
   fatherName: z.string(),
   fatherOccupation: z.string(),
   fatherContactNo: z.string(),
@@ -21,18 +21,18 @@ const guardianSchema = z.object({
   motherContactNo: z.string(),
 });
 
-const localGuardianSchema = z.object({
+const localGuardianValidationSchema = z.object({
   name: z.string(),
   occupation: z.string(),
   contactNo: z.string(),
   address: z.string(),
 });
 
-export const studentValidationSchema = z.object({
+export const createStudentValidationSchema = z.object({
   body: z.object({
     password: z.string().max(20),
     student: z.object({
-      name: userNameSchema,
+      name: userNameValidationSchema,
       gender: z.enum(['male', 'female', 'other']),
       dateOfBirth: z.string(),
       email: z.string().email(),
@@ -41,8 +41,8 @@ export const studentValidationSchema = z.object({
       bloogGroup: z.enum(['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']),
       presentAddress: z.string(),
       permanentAddress: z.string(),
-      guardian: guardianSchema,
-      localGuardian: localGuardianSchema,
+      guardian: guardianValidationSchema,
+      localGuardian: localGuardianValidationSchema,
       profileImg: z.string(),
     })
 
@@ -50,5 +50,5 @@ export const studentValidationSchema = z.object({
 });
 
 export const studentValidations = {
-  studentValidationSchema,
+ createStudentValidationSchema,
 };
--- src/app/modules/users/user.route.ts
+++ src/app/modules/users/user.route.ts
@@ -1,12 +1,12 @@
 import express from 'express'
 import { UserControllers } from './user.controller'
-import { studentValidationSchema } from '../student/student.validation'
+import { createStudentValidationSchema } from '../student/student.validation'
 import validationRequest from '../../middleware/validateRequest'
 
 const router = express.Router()
 
 
 
-router.post('/create-student', validationRequest(studentValidationSchema), UserControllers.createStudent)
+router.post('/create-student', validationRequest(createStudentValidationSchema), UserControllers.createStudent)
 
 export const userRoute = router
