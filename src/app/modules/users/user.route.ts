dae07aee682d80257c92518e61f74f291c1f6784
saikat <tawhidulislam3482@gmail.com>
2024-05-29 10:14:16 +0600

    middle ware

--- src/app/modules/users/user.route.ts
+++ src/app/modules/users/user.route.ts
@@ -1,8 +1,14 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import { UserControllers } from './user.controller'
 
 const router = express.Router()
 
-router.post('/create-student', UserControllers.createStudent)
+const checkValidation = (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.body)
+    next()
+}
+
+
+router.post('/create-student', checkValidation, UserControllers.createStudent)
 
 export const userRoute = router
