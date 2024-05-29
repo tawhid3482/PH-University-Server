f9efa6e16709b420b9e2c379e24413608b2fbf3f
saikat <tawhidulislam3482@gmail.com>
2024-05-29 11:01:23 +0600

    validate Request

---
+++ src/app/middleware/validateRequest.ts
@@ -0,0 +1,20 @@
+import  { NextFunction, Request, Response } from 'express'
+
+import { AnyZodObject } from "zod"
+
+const validationRequest = (schema: AnyZodObject) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        try{
+            
+        // validation
+        await schema.parseAsync({
+            body: req.body
+        })
+
+        next()
+        }catch(err){
+            next(err)
+        }
+    }
+}
+export default validationRequest
--- src/app/modules/users/user.route.ts
+++ src/app/modules/users/user.route.ts
@@ -1,14 +1,12 @@
-import express, { NextFunction, Request, Response } from 'express'
+import express from 'express'
 import { UserControllers } from './user.controller'
+import { studentValidationSchema } from '../student/student.validation'
+import validationRequest from '../../middleware/validateRequest'
 
 const router = express.Router()
 
-const checkValidation = (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.body)
-    next()
-}
 
 
-router.post('/create-student', checkValidation, UserControllers.createStudent)
+router.post('/create-student', validationRequest(studentValidationSchema), UserControllers.createStudent)
 
 export const userRoute = router
