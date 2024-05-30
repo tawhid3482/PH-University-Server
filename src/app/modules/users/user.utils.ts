cd9f62befab2330edfe00f9122ee8133d461e8c1
saikat <tawhidulislam3482@gmail.com>
2024-05-30 10:38:19 +0600

    academic semester services

--- src/app/modules/users/user.utils.ts
+++ src/app/modules/users/user.utils.ts
@@ -9,11 +9,21 @@ const findLastStudentId = async (): Promise<string | undefined> => {
     .sort({ createdAt: -1 })
     .lean();
 
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 export const generateStudentId = async (payload: TAcademicSemester): Promise<string> => {
-  const currentId = (await findLastStudentId()) || (0).toString();
+  let currentId = (0).toString(); // by default 0000
+
+  const lastStudentId = await findLastStudentId();
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6) //01
+  const lastStudentYear = lastStudentId?.substring(0, 4); //2030
+  const currentSemesterCode = payload.code;
+  const currentSemesterYear = payload.year;
+
+  if ((lastStudentId && lastStudentSemesterCode === currentSemesterCode && lastStudentYear === currentSemesterYear)) {
+    currentId = lastStudentId.substring(6)
+  }
   const incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
   return `${payload.year}${payload.code}${incrementId}`;
 };
