d1850caa2e924dbe6ea6fd0fd2d2aedfaaa42a10
saikat <tawhidulislam3482@gmail.com>
2024-05-31 21:42:45 +0600

    global error handler

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -2,18 +2,26 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { ErrorRequestHandler, NextFunction, Request, Response, } from 'express';
 
-const globalErrorHandler:ErrorRequestHandler = (
+const globalErrorHandler: ErrorRequestHandler = (
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    err,
-    req,
-    res,
-    next) => {
+    err, req, res, next) => {
+    // default values
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Something went wrong!';
+    type TErrorSource = {
+        path: string | number;
+        message: string
+    }[]
+    const errorSource: TErrorSource = [{
+        path: '',
+        message: 'Something went wrong!'
+    }]
+
     return res.status(statusCode).json({
         success: false,
         message,
-        error: err
+        errorSource,
+        amiError:err
     })
 
 }
