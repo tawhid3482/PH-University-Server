b0dce564ffb0e6c4f9212fb0d061958d4c666a0d
saikat <tawhidulislam3482@gmail.com>
2024-05-31 21:49:34 +0600

    global error handler

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,13 +1,14 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { ErrorRequestHandler, NextFunction, Request, Response, } from 'express';
+import { ZodError } from 'zod';
 
 const globalErrorHandler: ErrorRequestHandler = (
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     err, req, res, next) => {
     // default values
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Something went wrong!';
+    let statusCode = err.statusCode || 500;
+    let message = err.message || 'Something went wrong!';
     type TErrorSource = {
         path: string | number;
         message: string
@@ -17,11 +18,21 @@ const globalErrorHandler: ErrorRequestHandler = (
         message: 'Something went wrong!'
     }]
 
+    if (err instanceof ZodError) {
+        statusCode = 404;
+        message = 'ami zod tumi ki'
+    };
+
+
+
+
+
+
     return res.status(statusCode).json({
         success: false,
         message,
         errorSource,
-        amiError:err
+        Error: err
     })
 
 }
