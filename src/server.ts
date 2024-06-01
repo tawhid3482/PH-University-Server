81cc5e60f5076e6d14b30d6ea0d0edfa1ab64503
saikat <tawhidulislam3482@gmail.com>
2024-06-01 21:24:05 +0600

    okey app and  server

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import mongoose from 'mongoose';
 import app from './app';
 import config from './app/config';
+import { Server } from 'http'
+let server: Server;
 
-async function main() {
+ async function main() {
   try {
     await mongoose.connect(config.database_url as string);
 
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`app is listening on port ${config.port}`);
     });
   } catch (err) {
@@ -15,3 +17,15 @@ async function main() {
 }
 
 main();
+
+process.on('unhandledRejection', ()=>{
+  if(server){
+    server.close(()=>{
+      process.exit(1)
+    })
+  }
+})
+
+process.on('uncaughtException',()=>{
+  process.exit(1)
+})
