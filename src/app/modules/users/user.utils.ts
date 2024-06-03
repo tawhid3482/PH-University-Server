618c4663e67d747acddced8e4b92d0951db3db57
saikat <tawhidulislam3482@gmail.com>
2024-06-03 19:35:34 +0600

    Admin

--- src/app/modules/users/user.utils.ts
+++ src/app/modules/users/user.utils.ts
@@ -1,29 +1,112 @@
 import { TAcademicSemester } from "../academicSemester/academicSemester.interface";
 import { User } from "./user.model";
 
-const findLastStudentId = async (): Promise<string | undefined> => {
+const findLastStudentId = async () => {
   const lastStudent = await User.findOne(
-    { role: 'student' },
-    { id: 1, _id: 0 }
+    {
+      role: 'student',
+    },
+    {
+      id: 1,
+      _id: 0,
+    },
   )
-    .sort({ createdAt: -1 })
+    .sort({
+      createdAt: -1,
+    })
     .lean();
 
   return lastStudent?.id ? lastStudent.id : undefined;
 };
 
-export const generateStudentId = async (payload: TAcademicSemester): Promise<string> => {
-  let currentId = (0).toString(); // by default 0000
-
+export const generateStudentId = async (payload: TAcademicSemester) => {
+  let currentId = (0).toString();
   const lastStudentId = await findLastStudentId();
-  const lastStudentSemesterCode = lastStudentId?.substring(4, 6) //01
-  const lastStudentYear = lastStudentId?.substring(0, 4); //2030
-  const currentSemesterCode = payload.code;
-  const currentSemesterYear = payload.year;
 
-  if ((lastStudentId && lastStudentSemesterCode === currentSemesterCode && lastStudentYear === currentSemesterYear)) {
-    currentId = lastStudentId.substring(6)
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
+  const lastStudentYear = lastStudentId?.substring(0, 4);
+
+  const currentSemesterCode = payload.code;
+  const currentYear = payload.year;
+
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentYear === currentYear
+  ) {
+    currentId = lastStudentId.substring(6);
   }
-  const incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
-  return `${payload.year}${payload.code}${incrementId}`;
+
+  let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
+
+  incrementId = `${payload.year}${payload.code}${incrementId}`;
+
+  return incrementId;
+};
+
+// Faculty ID
+export const findLastFacultyId = async () => {
+  const lastFaculty = await User.findOne(
+    {
+      role: 'faculty',
+    },
+    {
+      id: 1,
+      _id: 0,
+    },
+  )
+    .sort({
+      createdAt: -1,
+    })
+    .lean();
+
+  return lastFaculty?.id ? lastFaculty.id.substring(2) : undefined;
+};
+
+export const generateFacultyId = async () => {
+  let currentId = (0).toString();
+  const lastFacultyId = await findLastFacultyId();
+
+  if (lastFacultyId) {
+    currentId = lastFacultyId.substring(2);
+  }
+
+  let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
+
+  incrementId = `F-${incrementId}`;
+
+  return incrementId;
+};
+
+// Admin ID
+export const findLastAdminId = async () => {
+  const lastAdmin = await User.findOne(
+    {
+      role: 'admin',
+    },
+    {
+      id: 1,
+      _id: 0,
+    },
+  )
+    .sort({
+      createdAt: -1,
+    })
+    .lean();
+
+  return lastAdmin?.id ? lastAdmin.id.substring(2) : undefined;
+};
+
+export const generateAdminId = async () => {
+  let currentId = (0).toString();
+  const lastAdminId = await findLastAdminId();
+
+  if (lastAdminId) {
+    currentId = lastAdminId.substring(2);
+  }
+
+  let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
+
+  incrementId = `A-${incrementId}`;
+  return incrementId;
 };
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -4,6 +4,8 @@ import { userRoute } from "../modules/users/user.route";
 import { AcademicSemesterRoutes } from "../modules/academicSemester/academicSemester.route";
 import { AcademicFacultyRoutes } from "../modules/academicFaculty/academicFaculty.route";
 import { AcademicDepartmentRoutes } from "../modules/academicDepartment/academicDepartment.route";
+import { FacultyRoutes } from "../modules/Faculty/faculty.route";
+import { AdminRoutes } from "../modules/Admin/admin.route";
 
 const router = Router()
 
@@ -27,6 +29,13 @@ const moduleRoutes = [
     {
         path: '/academic-departments',
         route: AcademicDepartmentRoutes
+    }, {
+        path: '/faculties',
+        route: FacultyRoutes,
+    },
+    {
+        path: '/admins',
+        route: AdminRoutes,
     },
 ]
 
