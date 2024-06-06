0f1bbadf67303e0dbe1dfbd4e12d6d7acb165542
saikat <tawhidulislam3482@gmail.com>
2024-06-06 09:29:05 +0600

    semesterRegistration controller get single data

--- src/app/modules/semesterRegistration/semesterRegistration.controller.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.controller.ts
@@ -36,10 +36,27 @@ const createSemesterRegistration = catchAsync(
     },
   );
 
-
+  const getSingleSemesterRegistration = catchAsync(
+    async (req: Request, res: Response) => {
+      const { id } = req.params;
+  
+      const result =
+        await SemesterRegistrationService.getSingleSemesterRegistrationsFromDB(
+          id,
+        );
+  
+      sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Semester Registration is retrieved successfully',
+        data: result,
+      });
+    },
+  );
 
   export const SemesterRegistrationController = {
     createSemesterRegistration,
     getAllSemesterRegistrations,
+    getSingleSemesterRegistration,
    
   };
