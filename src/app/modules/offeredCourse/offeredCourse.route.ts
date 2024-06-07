5a58e2bf0cc767c2f3f68ad4df98a90bffe5d618
tawhidul islam <tawhidulislam3482@gmail.com>
2024-06-07 19:28:10 +0600

    offeredCourse route

---
+++ src/app/modules/offeredCourse/offeredCourse.route.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+
+
+const router = express.Router();
+
+router.get('/', OfferedCourseControllers.getAllOfferedCourses);
+
+router.get('/:id', OfferedCourseControllers.getSingleOfferedCourses);
+
+router.post(
+  '/create-offered-course',
+  validateRequest(OfferedCourseValidations.createOfferedCourseValidationSchema),
+  OfferedCourseControllers.createOfferedCourse,
+);
+
+router.patch(
+  '/:id',
+  validateRequest(OfferedCourseValidations.updateOfferedCourseValidationSchema),
+  OfferedCourseControllers.updateOfferedCourse,
+);
+
+router.delete(
+  '/:id',
+  OfferedCourseControllers.deleteOfferedCourseFromDB,
+);
+
+export const offeredCourseRoutes = router;
