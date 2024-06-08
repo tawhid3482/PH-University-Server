50522c68b7c71f44649a5b952d3ca82c6b8511da
tawhidul islam <tawhidulislam3482@gmail.com>
2024-06-08 20:15:55 +0600

    auth controller

---
+++ src/app/modules/Auth/auth.controller.ts
@@ -0,0 +1,27 @@
+import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import config from '../../config';
+import sendResponse from '../../utils/sendResponse';
+
+const loginUser = catchAsync(async (req, res) => {
+  const result = await AuthServices.loginUser(req.body);
+  const { refreshToken, accessToken, needsPasswordChange } = result;
+
+  res.cookie('refreshToken', refreshToken, {
+    secure: config.NODE_ENV === 'production',
+    httpOnly: true,
+  });
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'User is logged in succesfully!',
+    data: {
+      accessToken,
+      needsPasswordChange,
+    },
+  });
+});
+export const AuthControllers = {
+  loginUser,
+};
