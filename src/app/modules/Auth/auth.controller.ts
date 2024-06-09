bb297d846eea9b19ccb0e8d14335f09391bdeabc
tawhidul islam <tawhidulislam3482@gmail.com>
2024-06-09 06:46:04 +0600

    auth controller

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -23,6 +23,22 @@ const loginUser = catchAsync(async (req, res) => {
     },
   });
 });
+
+const changePassword = catchAsync(async (req, res) => {
+  const { ...passwordData } = req.body;
+
+  const result = await AuthServices.changePassword(req.user, passwordData);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Password is updated succesfully!',
+    data: result,
+  });
+});
+
+
+
 export const AuthControllers = {
   loginUser,
+  changePassword
 };
