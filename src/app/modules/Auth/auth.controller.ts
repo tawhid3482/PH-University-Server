22936bea9a10f8e2de5dad765b50cdaae9b796bc
tawhidul islam <tawhidulislam3482@gmail.com>
2024-06-09 06:46:35 +0600

    auth controller

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -37,8 +37,20 @@ const changePassword = catchAsync(async (req, res) => {
 });
 
 
+const refreshToken = catchAsync(async (req, res) => {
+  const { refreshToken } = req.cookies;
+  const result = await AuthServices.refreshToken(refreshToken);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Access token is retrieved succesfully!',
+    data: result,
+  });
+});
 
 export const AuthControllers = {
   loginUser,
-  changePassword
+  changePassword,
+  refreshToken
 };
