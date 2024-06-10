3499cb3ea9a5fb32264ea67b68b260b1cb0e5424
tawhidul islam <tawhidulislam3482@gmail.com>
2024-06-10 16:16:58 +0600

    auth validation

--- src/app/modules/Auth/auth.route.ts
+++ src/app/modules/Auth/auth.route.ts
@@ -1,22 +1,25 @@
 import express from 'express';
+import validationRequest from '../../middleware/validateRequest';
+import { AuthValidation } from './auth.validation';
+import { AuthControllers } from './auth.controller';
 const router = express.Router();
 
 router.post(
   '/login',
-  validateRequest(AuthValidation.loginValidationSchema),
+  validationRequest(AuthValidation.loginValidationSchema),
   AuthControllers.loginUser,
 );
 
 router.post(
   '/change-password',
   auth(USER_ROLE.admin, USER_ROLE.faculty, USER_ROLE.student),
-  validateRequest(AuthValidation.changePasswordValidationSchema),
+  validationRequest(AuthValidation.changePasswordValidationSchema),
   AuthControllers.changePassword,
 );
 
 router.post(
   '/refresh-token',
-  validateRequest(AuthValidation.refreshTokenValidationSchema),
+  validationRequest(AuthValidation.refreshTokenValidationSchema),
   AuthControllers.refreshToken,
 );
 
